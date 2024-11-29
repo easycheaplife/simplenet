20e4a8d50ee7ea8f4a245d3dd0be912bd472692e
root <lijin@163.com>
2024-11-29 07:14:06 +0000

    del log file

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 g++ -g -o server epoll_reactor.cc event_loop.cc select_reactor.cc server.cc -lpthread
+g++ -g -o echo_server epoll_reactor.cc event_loop.cc select_reactor.cc echo_server.cc -lpthread
 g++ -g -o client test_client.cc
